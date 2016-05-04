a362f20554c21a51ed425f5f408b30d7e279452a
Juan <jurique@alumnos.ubiobio.cl>
2016-05-04 04:16:57 -0400

    JuanRiquelme

--- protected/extensions/heart/views/layouts/_menu.php
+++ protected/extensions/heart/views/layouts/_menu.php
@@ -10,16 +10,16 @@ $menu=
 		array('label'=>'Home', 'url'=>array('/site/index'), 'icon'=>'fa fa-home','active'=>($currController=='site' and $currAction=='index' )),
 		array('label'=>'Cuentas de usuario', 'url'=>'#', 'icon'=>'fa fa-users', 'visible'=>!Yii::app()->user->isGuest, 'active'=> false ,'items'=>array(
 			array('label'=>'Administrador', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest,'items'=>array(
-				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=administrador/create', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>'Ver cuentas', 'url'=>'index.php?r=administrador/index', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=administrador/create', 'icon'=>'fa fa-user-plus', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Ver cuentas', 'url'=>'index.php?r=administrador/index', 'icon'=>'fa fa-file-text-o', 'visible'=>!Yii::app()->user->isGuest),
 			)),
 			array('label'=>'Encargado Cocina', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest,'items'=>array(
-				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=encargado_cocina/create', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>'Ver cuentas', 'url'=>'index.php?r=encargado_cocina/index', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=encargado_cocina/create', 'icon'=>'fa fa-user-plus', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Ver cuentas', 'url'=>'index.php?r=encargado_cocina/index', 'icon'=>'fa fa-file-text-o', 'visible'=>!Yii::app()->user->isGuest),
 			)),
 			array('label'=>'Repartidor', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest,'items'=>array(
-				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=repartidor/create', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>'Ver cuentas', 'url'=>'index.php?r=repartidor/index', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Crear nueva cuenta', 'url'=>'index.php?r=repartidor/create', 'icon'=>'fa fa-user-plus', 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Ver cuentas', 'url'=>'index.php?r=repartidor/index', 'icon'=>'fa fa-file-text-o', 'visible'=>!Yii::app()->user->isGuest),
 			)),
 		)),
 		array('label'=>'Admin', 'url'=>'#', 'icon'=>'fa fa-gear', 'visible'=>!Yii::app()->user->isGuest, 'active'=> false ,'items'=>array(
