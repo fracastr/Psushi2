b55d053ba4b0584ff1c656d554bd88b887b20c09
Juan <jurique@alumnos.ubiobio.cl>
2016-05-04 04:06:55 -0400

    JuanRiquelme

--- protected/extensions/heart/views/layouts/_menu.php
+++ protected/extensions/heart/views/layouts/_menu.php
@@ -12,15 +12,15 @@ $menu=
 			array('label'=>'Administrador', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),'items'=>array(
 				array('label'=>'Crear nueva cuenta', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Ver cuentas', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-			),
+			)),
 			array('label'=>'Encargado Cocina', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),'items'=>array(
 				array('label'=>'Crear nueva cuenta', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Ver cuentas', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-			),
+			)),
 			array('label'=>'Repartidor', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),'items'=>array(
 				array('label'=>'Crear nueva cuenta', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Ver cuentas', 'url'=>'#', 'icon'=>'fa fa-user', 'visible'=>!Yii::app()->user->isGuest),
-			),
+			)),
 		)),
 		array('label'=>'Admin', 'url'=>'#', 'icon'=>'fa fa-gear', 'visible'=>!Yii::app()->user->isGuest, 'active'=> false ,'items'=>array(
 			array('label'=>'Generator Code', 'url'=>array('/gii/heart'), 'icon'=>'fa fa-refresh fa-fw', 'visible'=>!Yii::app()->user->isGuest),
