153e91cfaeb5b191152498488bd74907979ef264
Juan <jurique@alumnos.ubiobio.cl>
2016-05-04 00:30:39 -0400

    Juan Riquelme

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,8 +29,8 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Hoe', 'url'=>array('/site/index')),
-				array('label'=>'GII','url'=>array('?r=gii')),
+				array('label'=>'Home', 'url'=>array('/site/index')),
+				
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
