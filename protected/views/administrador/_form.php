5baf91fc272818215e02bd63906919fb50e3dfdc
Juan <jurique@alumnos.ubiobio.cl>
2016-05-09 16:00:23 -0400

    Juan Riquelme

--- protected/views/administrador/_form.php
+++ protected/views/administrador/_form.php
@@ -8,7 +8,7 @@
 	// 'htmlOptions'=>array('enctype'=>'multipart/form-data'),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Los campos marcados con <span class="required">*</span> son obligatorios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
