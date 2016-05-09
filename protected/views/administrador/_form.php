0bf6f10018622007dd192c889cea59932f1922bb
Juan <jurique@alumnos.ubiobio.cl>
2016-05-09 16:02:46 -0400

    Juan Riquelme

--- protected/views/administrador/_form.php
+++ protected/views/administrador/_form.php
@@ -11,20 +11,24 @@
 	<p class="note">Los campos marcados con <span class="required">*</span> son obligatorios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
-
-<?php echo $form->textFieldRow($model,'RUT_PERSONAL',array('class'=>'span5','maxlength'=>15)); ?>
-<?php echo $form->textFieldRow($model,'ID_SUCURSAL',array('class'=>'span5')); ?>
-<?php echo $form->textFieldRow($model,'NOMBRE_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
-<?php echo $form->textFieldRow($model,'PATERNO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
-<?php echo $form->textFieldRow($model,'MATERNO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
-<?php echo $form->textFieldRow($model,'CONTRASENA_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
-<?php echo $form->textFieldRow($model,'SEXO_PERSONAL',array('class'=>'span5','maxlength'=>1)); ?>
-<?php echo $form->textFieldRow($model,'TELEFONO_PERSONAL',array('class'=>'span5')); ?>
-<?php echo $form->textFieldRow($model,'DIRECCION_PERSONAL',array('class'=>'span5','maxlength'=>50)); ?>
-<?php echo $form->textFieldRow($model,'EMAIL_PERSONAL',array('class'=>'span5','maxlength'=>100)); ?>
-<?php echo $form->textFieldRow($model,'AUTORIZADO_PERSONAL',array('class'=>'span5')); ?>
-<?php echo $form->textFieldRow($model,'CARGO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
-
+<table>
+	<tr>
+		<td width="300">
+		<?php echo $form->textFieldRow($model,'RUT_PERSONAL',array('class'=>'span5','maxlength'=>15)); ?>
+		</td>
+	</tr>
+	<?php echo $form->textFieldRow($model,'ID_SUCURSAL',array('class'=>'span5')); ?>
+	<?php echo $form->textFieldRow($model,'NOMBRE_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
+	<?php echo $form->textFieldRow($model,'PATERNO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
+	<?php echo $form->textFieldRow($model,'MATERNO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
+	<?php echo $form->textFieldRow($model,'CONTRASENA_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
+	<?php echo $form->textFieldRow($model,'SEXO_PERSONAL',array('class'=>'span5','maxlength'=>1)); ?>
+	<?php echo $form->textFieldRow($model,'TELEFONO_PERSONAL',array('class'=>'span5')); ?>
+	<?php echo $form->textFieldRow($model,'DIRECCION_PERSONAL',array('class'=>'span5','maxlength'=>50)); ?>
+	<?php echo $form->textFieldRow($model,'EMAIL_PERSONAL',array('class'=>'span5','maxlength'=>100)); ?>
+	<?php echo $form->textFieldRow($model,'AUTORIZADO_PERSONAL',array('class'=>'span5')); ?>
+	<?php echo $form->textFieldRow($model,'CARGO_PERSONAL',array('class'=>'span5','maxlength'=>30)); ?>
+</table>
 
 <div class="form-actions">
 	<?php $this->widget('bootstrap.widgets.TbButton', array(
